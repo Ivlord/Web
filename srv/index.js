a1c2dc7ea379ca3735d8d0564f172fcaef15fc9c
Mazlov Ivan <ivlord@inbox.ru>
2022-11-29 04:33:36 +0300

    srv tst2

--- srv/index.js
+++ srv/index.js
@@ -1,43 +1,11 @@
-console.log('hi')
+
 const requestURL = 'https://ivlord.github.io/Web/srv/srv.html?q=10&iv=30'
 
 function getRq(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest()
-        xhr.open('GET', url)
-        xhr.responseType = 'json'
-        xhr.setRequestHeader('Content-Type', 'application/json')
-
-        xhr.onload = () => {
-            if (xhr.status >= 400) {
-                console.log("1>", xhr.response)
-                reject( xhr.response) }
-            else                   {
-                console.log("2>", xhr.response)
-                resolve(xhr.response) }
-        }
-
-        xhr.onerror = () => { reject(xhr.response) }
-
-        xhr.send()
-    })
-}
-
-let res = getRq(requestURL)
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-
-
-
-
-
-
-/*
-function getRq(method, url) {
     const headers = {'Content-Type':'application/json'}
 
     return fetch(url, {
-        method: method,
+        method: 'GET',
         headers: headers
     }).then(response => {
         if (response.ok) { return response.json() }
@@ -50,19 +18,8 @@ function getRq(method, url) {
     })
 }
 
-let res = getRq('GET', requestURL)
+let res = getRq(requestURL)
     .then(data => console.log(data))
     .catch(err => console.log(err))
 
 console.log(res)
-
-
-const body = {
-    name: 'Vladilen',
-    age: 26
-}
-
-sendRequest('POST', requestURL, body)
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-*/
